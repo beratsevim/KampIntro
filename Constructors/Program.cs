1cdae0a695c9c2d0f13dff2dfac3a9d77f0f0146
BERAT SEVİM <berat.svm@hotmail.com>
2022-05-17 18:17:19 +0300

    17 Mayıs

---
+++ Constructors/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer{ Id = 1, FirstName = "Engin", LastName = "Demiroğ", City = "Ankara" };
+            Customer customer2 = new Customer(2,"Derin","Demiroğ","Ankara");
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.FirstName = "Berat";
+            customer3.LastName = "Sevim";
+            customer3.City = "Bursa";
+
+            Console.WriteLine(customer2.FirstName);
+            
+
+
+
+        }
+    }
+    class Customer
+    {
+        //default constructor
+        public Customer()
+        {
+
+        }
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+        public int Id{ get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ DictionaryIntro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DictionaryIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+            isimler.Add("Berat");
+            isimler.Add("Emirhan");
+            isimler.Add("Batu");
+            isimler.Add("Harun");
+            isimler.Add("Berat");
+            isimler.Add("Emirhan");
+            isimler.Add("Batu");
+            isimler.Add("Harun");
+            Console.WriteLine(isimler.Count);
+            
+        }
+    }
+
+    class MyDictionary<T> 
+    {
+        T[] array;
+        T[] tempArray;
+        public MyDictionary()
+        {
+            array = new T[0];
+        }
+        public void Add(T item)
+        {
+            tempArray = array;
+            array = new T[array.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                array[i] = tempArray[i];
+            }
+            array[array.Length - 1] = item; 
+        }
+        public int Count
+        {
+            get { return array.Length; }
+
+        }
+
+    }
+}
